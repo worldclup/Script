bfeff66b0ddfd261bc968bf74a0eafc47a9c0d04
Jetniphat <jetniphat@MacBook-Pro-khxng-Jetniphat.local>
2025-12-25 01:58:10 +0700

    Fixed version

--- new-main-mega-boss.lua
+++ new-main-mega-boss.lua
@@ -189,7 +189,7 @@ local Window = UI:CreateWindow({
 
 do
     Window:Tag({
-        Title = "v1.0.1",
+        Title = "v1.1.1",
         Icon = "github",
         Color = Color3.fromHex("#50C878")
     })
