7a88ad78504142ef4432c3a0b38b2a9c2d40d3a8
Jetniphat <jetniphat@MacBook-Pro-khxng-Jetniphat.local>
2026-01-05 21:38:09 +0700

    Fixed code

--- new-main-chrismas-copy.lua
+++ new-main-chrismas-copy.lua
@@ -39,6 +39,15 @@ local RunService = game:GetService("RunService");
 local Reliable = (ReplicatedStorage:WaitForChild("Reply")):WaitForChild("Reliable");
 local Unreliable = (ReplicatedStorage:WaitForChild("Reply")):WaitForChild("Unreliable");
 ------------------------------------------------------------------------------------
+--- Anti-AFK System
+------------------------------------------------------------------------------------
+local VirtualUser = game:GetService("VirtualUser")
+LocalPlayer.Idled:Connect(function()
+    VirtualUser:CaptureController()
+    VirtualUser:ClickButton2(Vector2.new())
+    warn("Anti-AFK System: Active!") -- แจ้งเตือนใน Console ว่าระบบทำงาน
+end)
+------------------------------------------------------------------------------------
 --- Module
 ------------------------------------------------------------------------------------
 local ConfigsPath = ReplicatedStorage.Scripts.Configs;
