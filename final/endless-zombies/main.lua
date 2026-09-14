3cd2fe1c24f652fb629e5c63b4e4f58d998ea5d2
Jetniphat Panphol <jetniphatpanphol@Jetniphats-MacBook-Pro.local>
2026-09-14 16:39:39 +0700

    Fix: logo

--- final/endless-zombies/main.lua
+++ final/endless-zombies/main.lua
@@ -189,7 +189,7 @@ end
 
 local Window = Rayfield:CreateWindow({
 	name = "DEK DEV HUB", subtitle = "Endless Zombie",
-	sidebarLayout = true, theme = "default", showName = "DEK", showIconOnly = true,
+	sidebarLayout = true, theme = "default", showName = "DEK", showIcon = "rbxassetid://97773063258281", showIconOnly = true,
 })
 local Tabs = {
 	Main = Window:CreateTab({ name = "Main" }),
